3fad9553f062812672e06c8b533d4e7c8ae6aafd
Takayuki Miyahara <voyager.3taka28@gmail.com>
2023-12-08 01:00:41 +0900

    feat: logger

--- server.go
+++ server.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"net/http"
+	"os"
+
+	"log/slog"
 )
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout,
+		&slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}),
+	)
+
 	srv := &http.Server{
 		Addr: ":8080",
 	}
-	if err := srv.ListenAndServe(); err != nil {
-		panic(err)
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		logger.Error("server ListenAndServe error: ", err)
 	}
 }
