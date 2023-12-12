a8d6e3c6d8d859b6ca92637c38ac900f40e101a4
Takayuki Miyahara <voyager.3taka28@gmail.com>
2023-12-13 02:32:57 +0900

    feat: radix tree

---
+++ radix_tree.go
@@ -0,0 +1,54 @@
+// highly inspired by jun06t
+// cf. https://christina04.hatenablog.com/entry/routing-with-radix-tree
+// cf. https://github.com/jun06t/go-sample/tree/master/radix-tree
+// MIT License
+
+package api
+
+import "strings"
+
+type Node struct {
+	Part     string
+	Children []*Node
+	IsWild   bool
+	Route    Route
+}
+
+func (n *Node) insert(parttern string, route Route) {
+	parts := strings.Split(parttern, "/")[1:]
+
+	for _, part := range parts {
+		child := n.matchChild(part)
+		if child == nil {
+			child = &Node{
+				Part:   part,
+				IsWild: part[0] == ':' || part[0] == '*' || (part[0] == '{' && part[len(part)-1] == '}'),
+			}
+			n.Children = append(n.Children, child)
+		}
+		n = child
+	}
+	n.Route = route
+}
+
+func (n *Node) matchChild(part string) *Node {
+	for _, child := range n.Children {
+		if child.Part == part || child.IsWild {
+			return child
+		}
+	}
+	return nil
+}
+
+func (n *Node) search(path string) Route {
+	parts := strings.Split(path, "/")[1:]
+
+	for _, part := range parts {
+		child := n.matchChild(part)
+		if child == nil {
+			return Route{}
+		}
+		n = child
+	}
+	return n.Route
+}
