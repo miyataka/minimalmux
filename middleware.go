672c3b1e6680c3e04ed2c2840fd2f35ba2c5cf5a
Takayuki Miyahara <voyager.3taka28@gmail.com>
2023-12-24 22:37:09 +0900

    feat: middlewares

---
+++ middleware.go
@@ -0,0 +1,33 @@
+package api
+
+import "net/http"
+
+type Middlewares []func(http.Handler) http.Handler
+
+func NewMiddlewares() *Middlewares {
+	return &Middlewares{}
+}
+
+func (m *Middlewares) Append(middleware func(http.Handler) http.Handler) *Middlewares {
+	*m = append(*m, middleware)
+	return m
+}
+
+func (m *Middlewares) Prepend(middleware func(http.Handler) http.Handler) *Middlewares {
+	*m = append(Middlewares{middleware}, *m...)
+	return m
+}
+
+func (m *Middlewares) Handle(h http.Handler) http.Handler {
+	if len(*m) == 0 {
+		return h
+	}
+	for i := len(*m) - 1; i >= 0; i-- {
+		h = (*m)[i](h)
+	}
+	return h
+}
+
+func (m *Middlewares) HandleFunc(h http.Handler) http.Handler {
+	return m.Handle(h)
+}
---
+++ middleware_test.go
@@ -0,0 +1,61 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func testMiddleware(s string) func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(fmt.Sprintf("%s\n", s)))
+			h.ServeHTTP(w, r)
+		})
+	}
+}
+
+var testHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("test\n"))
+})
+
+func TestAppend(t *testing.T) {
+	aFunc := testMiddleware("a")
+	bFunc := testMiddleware("b")
+
+	ms := NewMiddlewares()
+	ms = ms.Append(aFunc).Append(bFunc)
+
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ms.Handle(testHandler).ServeHTTP(w, r)
+
+	expected := "a\nb\ntest\n"
+	if w.Body.String() != expected {
+		t.Fatalf("expected %q, got %q", expected, w.Body.String())
+	}
+}
+
+func TestPrepend(t *testing.T) {
+	aFunc := testMiddleware("a")
+	bFunc := testMiddleware("b")
+
+	ms := NewMiddlewares()
+	ms = ms.Prepend(aFunc).Prepend(bFunc)
+
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ms.Handle(testHandler).ServeHTTP(w, r)
+
+	expected := "b\na\ntest\n"
+	if w.Body.String() != expected {
+		t.Fatalf("expected %q, got %q", expected, w.Body.String())
+	}
+}
