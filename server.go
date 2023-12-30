866b637c2fa419d33c6edca1be47820dbae5dc87
Takayuki Miyahara <voyager.3taka28@gmail.com>
2023-12-30 14:41:28 +0900

    feat: graceful shutdown

--- server.go
+++ server.go
@@ -1,9 +1,14 @@
 package api
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"log/slog"
 )
@@ -27,8 +32,39 @@ func RunServer() {
 		Handler: mux,
 	}
 
-	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-		logger.Error("server ListenAndServe error: ", err)
+	// prepare graceful shutdown
+	ctx, stop := signal.NotifyContext(context.Background(),
+		syscall.SIGTERM, os.Interrupt, os.Kill,
+	)
+	defer stop()
+	ctx, cancelCauseFunc := context.WithCancelCause(ctx)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			logger.Error("server ListenAndServe error: ", err)
+			cancelCauseFunc(err)
+		}
+	}()
+
+	<-ctx.Done() // wait signal
+
+	// shutdown
+	ctx, cancelFunc := context.WithTimeout(
+		context.Background(), // shutdown context
+		5*time.Second,        // TODO configurable
+	)
+	defer cancelFunc()
+
+	// shutdown server
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Error("server shutdown error: ", err)
+	} else {
+		logger.Info("server shutdown success")
+	}
+
+	// check timeout occurred or not
+	if err := context.Cause(ctx); err != nil && errors.Is(err, context.DeadlineExceeded) {
+		logger.Error("server shutdown timeout error", err)
 	}
 }
 
