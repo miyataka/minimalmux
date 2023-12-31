ece9a626caee76a0f7119ca9640ad313533b1821
Takayuki Miyahara <voyager.3taka28@gmail.com>
2023-12-31 14:29:30 +0900

    test: add case

--- mux_test.go
+++ mux_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestServeMuxRouting(t *testing.T) {
+func Test_handle(t *testing.T) {
 	tcs := []struct {
 		method       string
 		pattern      string
@@ -30,6 +30,38 @@ func TestServeMuxRouting(t *testing.T) {
 		testServer := httptest.NewServer(mux)
 		res := testHttpRequest(t, testServer, tc.method, tc.pattern)
 
+		if res.StatusCode != tc.expectStatus {
+			t.Errorf("Status code not equal. got: %d, want: %d", res.StatusCode, tc.expectStatus)
+		}
+	}
+}
+
+func Test_GetPostPubDeleteHeadOptionsPatchmethods(t *testing.T) {
+	tcs := []struct {
+		method       string
+		pattern      string
+		expectStatus int
+	}{
+		{method: http.MethodGet, pattern: "/test", expectStatus: http.StatusOK},
+		{method: http.MethodPost, pattern: "/test", expectStatus: http.StatusOK},
+		{method: http.MethodPut, pattern: "/test", expectStatus: http.StatusOK},
+		{method: http.MethodDelete, pattern: "/test", expectStatus: http.StatusOK},
+		{method: http.MethodHead, pattern: "/test", expectStatus: http.StatusOK},
+		{method: http.MethodOptions, pattern: "/test", expectStatus: http.StatusOK},
+		{method: http.MethodPatch, pattern: "/test", expectStatus: http.StatusOK},
+	}
+	mux := NewServeMux()
+	mux.Get("/test", testHandler)
+	mux.Post("/test", testHandler)
+	mux.Put("/test", testHandler)
+	mux.Delete("/test", testHandler)
+	mux.Head("/test", testHandler)
+	mux.Options("/test", testHandler)
+	mux.Patch("/test", testHandler)
+
+	for _, tc := range tcs {
+		testServer := httptest.NewServer(mux)
+		res := testHttpRequest(t, testServer, tc.method, tc.pattern)
 		if res.StatusCode != tc.expectStatus {
 			t.Errorf("Status code not equal. got: %d, want: %d", res.StatusCode, tc.expectStatus)
 		}
